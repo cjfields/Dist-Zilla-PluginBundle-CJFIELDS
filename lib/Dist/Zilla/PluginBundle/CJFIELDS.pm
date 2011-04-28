0d7ed812d153109f25c1da1aee18a1b7e3989ce8
Chris Fields <cjfields@bioperl.org>
2011-04-28 16:05:54 -0500

    start cleaning up a few things

--- lib/Dist/Zilla/PluginBundle/CJFIELDS.pm
+++ lib/Dist/Zilla/PluginBundle/CJFIELDS.pm
@@ -1,11 +1,57 @@
 package Dist::Zilla::PluginBundle::CJFIELDS;
 
-# ABSTRACT: Build your modules like CJFIELDS
+# ABSTRACT: Build your modules like CJFIELDS (not sure that's a recommendation)
 
 use Moose 1.0;
 use Moose::Util::TypeConstraints;
-use MooseX::Types::Moose qw(Bool Str ArrayRef);
+use MooseX::Types::Moose qw(Bool Str ArrayRef Any);
 use namespace::autoclean;
+
+=head1 SYNOPSIS
+
+In dist.ini:
+
+  [@CJFIELDS]
+  dist = Distribution-Name
+  repository_at = github
+
+=head1 DESCRIPTION
+
+This is the L<Dist::Zilla> configuration I use to build my distributions.
+It is an extension of L<Dist::Zilla::PluginBundle::FLORA>, but with a few
+configurable filters for my use.
+
+With default settings, it is roughly equivalent to:
+
+  @Basic
+
+  [MetaConfig]
+  [MetaJSON]
+  [PkgVersion]
+  [PodSyntaxTests]
+  [PodCoverageTests]
+  [NoTabsTests]
+  [EOLTests]
+  [NextRelease]
+
+  [MetaResources]
+  repository.type   = git
+  repository.url    = git://github.com/cjfields/${lowercase_dist}
+  repository.web    = http://github.com/cjfields/${lowercase_dist}
+  bugtracker.web    = http://rt.cpan.org/Public/Dist/Display.html?Name=${dist}
+  bugtracker.mailto = bug-${dist}@rt.cpan.org
+  homepage          = http://search.cpan.org/dist/${dist}
+
+  [Authority]
+  authority   = cpan:CJFIELDS
+  do_metadata = 1
+
+  [PodWeaver]
+  config_plugin = @FLORA ; using Florian's plugin here
+
+  [AutoPrereqs]
+
+=cut
 
 extends qw(Dist::Zilla::PluginBundle::FLORA);
 
@@ -16,13 +62,38 @@ has '+github_user'  => ( default     => 'cjfields' );
 has 'create_readme' => (
     isa         => Bool,
     is          => 'ro',
-    default     => 1
+    default     => 1,
+    trigger     => sub {
+        my ($self, $val) = @_;
+        if (!$val) {
+            
+        }
+        $val
+    }
 );
 
 has 'use_module_build'  => (
     isa         => Bool,
     is          => 'ro',
-    default     => 1
+    default     => 1,
+);
+
+has 'install_scripts'  => (
+    isa         => Bool,
+    is          => 'ro',
+    default     => 0
+);
+
+has 'scripts_dir'   =>(
+    isa         => 'Str',
+    is          => 'ro',
+    default     => 'scripts'
+);
+
+has 'skip_build'  => (
+    isa         => Bool,
+    is          => 'ro',
+    default     => 0
 );
 
 has 'use_next_release'  => (
@@ -31,12 +102,39 @@ has 'use_next_release'  => (
     default     => 1
 );
 
+has '_plugins_tbr'          => (
+    traits      => ['Array'],
+    isa         => ArrayRef[Str],
+    is          => 'bare',
+    init_arg    => undef,
+    handles     => {
+        'filter_plugin'     => 'push',
+        'filtered_plugins'  => 'elements'
+    }
+);
+
+has '_plugins_tba'               => (
+    traits      => ['Array'],
+    isa         => ArrayRef[Any],
+    is          => 'bare',
+    init_arg    => undef,
+    handles     => {
+        'add_config_plugin'     => 'push',
+        'config_plugins'  => 'elements'
+    }
+);
+
 override 'configure' => sub {
     my $self = shift;
 
+    Carp::croak("Can't set both skip_build and use_module_build")
+        if $self->skip_build && $self->use_module_build;
+
+    # these need to be moved into a trigger or builder or somethin'
     my @filtered;
     push @filtered, 'Readme' if !$self->create_readme;
     push @filtered, 'MakeMaker' if $self->use_module_build;
+    push @filtered, 'MakeMaker', 'ModuleBuild' if $self->skip_build;
 
     if (@filtered) {
         $self->add_bundle('@Filter' => {-bundle   => '@Basic',
@@ -69,14 +167,27 @@ override 'configure' => sub {
             do_metadata => 1,
         }],
         [EOLTests => {
-            trailing_whitespace => 1,
+            trailing_whitespace => !$self->disable_trailing_whitespace_tests,
         }],
     );
 
+    $self->add_plugins(['ExecDir' => {
+            scripts => $self->scripts_dir
+        }]
+        ) if $self->install_scripts;
+
+    $self->is_task
+        ? $self->add_plugins('TaskWeaver')
+        : $self->add_plugins(
+              [PodWeaver => {
+                  config_plugin => $self->weaver_config_plugin,
+              }],
+          );
+
+    # Same as above, these need to be moved into a trigger or builder or somethin'
+    $self->add_plugins('NextRelease') if $self->use_next_release;
     $self->add_plugins('ModuleBuild') if $self->use_module_build;
     $self->add_plugins('PodCoverageTests') if !$self->disable_pod_coverage_tests;
-    $self->add_plugins('NextRelease') if $self->use_next_release;
-
     $self->add_plugins('AutoPrereqs') if $self->auto_prereqs;
 };
 
